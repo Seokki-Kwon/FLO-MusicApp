76bbcab4f3642383a8c908713ed5950b1f530154
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-02-01 00:52:21 +0900

    Initial Commit

---
+++ Flo_CloneTests/Flo_CloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  Flo_CloneTests.swift
+//  Flo_CloneTests
+//
+//  Created by 권석기 on 2/1/25.
+//
+
+import Testing
+@testable import Flo_Clone
+
+struct Flo_CloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Flo_CloneUITests/Flo_CloneUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Flo_CloneUITestsLaunchTests.swift
+//  Flo_CloneUITests
+//
+//  Created by 권석기 on 2/1/25.
+//
+
+import XCTest
+
+final class Flo_CloneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
